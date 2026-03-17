376961d6275e2e71913f72caa37df16714584720
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-03-17 19:13:35 +0530

    Add files via upload

---
+++ rev num.py
@@ -0,0 +1,20 @@
+#Print numbers from 10 to 1 using a while loop
+
+# i=10
+# while i>=1:
+#     print(i)
+#     i-=1
+
+for i in range(10,0,-1):
+    print(i)
+
+
+#     0 → start
+
+# 0 → stop (stop before 0)
+
+# -1 → step (go backward)
+
+# i takes values: 10, 9, 8, … 1
+
+# print(i) prints each number
