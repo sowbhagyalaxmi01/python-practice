6016fbf883f1c5be5fab220ebc479c6fda0a9363
sowbhagyalaxmi01 <sowbhagyalaxmi1805@gmail.com>
2026-03-20 21:30:36 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,19 @@
+#Sum numbers from 1 to 10 using a for loop and print the result.
+
+# n = 10
+# sum = 0
+# for i in range(1, n+1):
+#     sum += i
+
+# print("Total sum =", sum)
+
+
+n = 10
+sum = 0
+i = 1
+while i <= n:
+    sum += i
+    i += 1
+
+print("Total sum =", sum)
+
